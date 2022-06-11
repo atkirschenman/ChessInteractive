ea4c15cc44260578812164169f263ea67db82453
Andrew Kirschenman <akirschenman@gmail.com>
2022-06-11 09:20:44 -0500

    TraverseAdded

---
+++ ChessInteractive/lib/Traverse/Traverse.cpp
@@ -0,0 +1,14 @@
+#include "Traverse.h"
+
+
+int AddSquare(ChessSquare* Head, int x, int y, int size)
+{
+
+}
+
+ChessSquare* FindSquare(ChessSquare* Head, int x, int y)
+{
+    ChessSquare* walker;
+    
+    Head
+}
