1e3cfa5cadd264f2a9d7ed069e49ed6fd10f42db
Andrew Kirschenman <akirschenman@gmail.com>
2022-06-18 16:21:10 -0500

    First Successful build!!

--- ChessInteractive/src/main.cpp
+++ ChessInteractive/src/main.cpp
@@ -1,13 +1,13 @@
 #include <Arduino.h>
 #include "ChessBoard.h"
 
-ChessBoard myBoard = new ChessBoard(8);
+ChessBoard* myBoard = new ChessBoard(8);
 
 void setup() {
   // put your setup code here, to run once:
 }
 
 void loop() {
-  myBoard.CleartheBoard();
+  myBoard->CleartheBoard();
   // put your main code here, to run repeatedly:
 } 
